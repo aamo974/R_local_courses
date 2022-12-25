0f7f534c6694fe00c09a22d72f66cd9f64d7ea46
Alex <alexandre.amouny@gmail.com>
2022-12-25 21:36:21 +0400

    Update gapminder.R

--- gapminder.R
+++ gapminder.R
@@ -7,6 +7,11 @@ data(gapminder)
 
 head(gapminder)
 
-gapminder %>%
-  filter(year == 2015 & country %in% c("Sri Lanka", "Turkey")) %>% 
-  select(country, infant_mortality)
+# gapminder %>%
+#   filter(year == 2015 & country %in% c("Sri Lanka", "Turkey")) %>% 
+#   select(country, infant_mortality)
+
+filter(gapminder, year == 1962) %>%
+  ggplot(aes(fertility, life_expectancy, color = continent)) +
+  geom_point()
+
