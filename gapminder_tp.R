47f5ac47ea9688cc20dd3fe8c6014de20421570e
Alex <alexandre.amouny@gmail.com>
2022-12-26 18:15:26 +0400

    Update gapminder_tp.R

--- gapminder_tp.R
+++ gapminder_tp.R
@@ -1,11 +1,7 @@
 library(dplyr)
-library(ggplot2)
 library(dslabs)
 data(gapminder)
 
 
-
-## fill out the missing parts in filter and aes
-gapminder %>% filter(continent == "Africa" & year == 2012) %>%
-  ggplot(aes(life_expectancy,fertility)) +
-  geom_point()
+df <- gapminder %>%
+  select(continent == "Africa" & year == 2012 & fertility <= 3)
